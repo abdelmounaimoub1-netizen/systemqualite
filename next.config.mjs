70db19cd7c47e049e719774db2aebd4c825ce6be
Ouballa10 <abdelmounaimoub12@gmail.com>
2026-06-12 02:33:56 +0100

    fix: remove --webpack flag from build, clean next.config for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  allowedDevOrigins: ["192.168.56.1"],
-  turbopack: {
-    root: process.cwd()
-  }
+  reactStrictMode: true
 };
 
 export default nextConfig;
